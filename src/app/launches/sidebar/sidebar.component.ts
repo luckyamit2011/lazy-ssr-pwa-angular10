b858b7ab2f9213d695b41afa359145d68f1c9aeb
luckyamit2011 <71408232+luckyamit2011@users.noreply.github.com>
2020-09-17 22:24:59 +0530

    Add files via upload

--- src/app/launches/sidebar/sidebar.component.ts
+++ src/app/launches/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Output,EventEmitter} from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -10,44 +10,40 @@ export class SidebarComponent implements OnInit {
 
   @Output() paramSelected = new EventEmitter<any>();
 
-  NoOfYears:number;
-  currentYear:number;
-  startYear:number = 2006;
-  programLimit:number;
-  selectedYear:string=''; selectedLaunch:string=''; selectedLand:string='';
+  NoOfYears: number;
+  currentYear: number;
+  startYear: number = 2006;
+  programLimit: number;
+  selectedYear: string = ''; selectedLaunch: string = ''; selectedLand: string = '';
 
-  constructor(private route: ActivatedRoute)
-   {  
-       route.queryParams.subscribe( (params) => {
-        if(params['limit'])
-        this.programLimit=params['limit']; //set from browser if page load occurs
-        else
-        this.programLimit=50;
-        if(params['launch_year'])
-        this.selectedYear =  params['launch_year'];
-        if(params['launch_success'])
-        this.selectedLaunch =  params['launch_success'];
-        if(params['land_success'])
-        this.selectedLand =  params['land_success'];
-        //console.warn(params);
-      });
-    }
+  constructor(private route: ActivatedRoute) {
+    route.queryParams.subscribe((params) => {
 
-    onfilterClick(key:string, value:string) {
+    if (params['launch_year'])
+    this.selectedYear = params['launch_year'];
+    if (params['launch_success'])
+    this.selectedLaunch = params['launch_success'];
+    if (params['land_success'])
+    this.selectedLand = params['land_success'];
 
-      if(key == 'launch_year')
-      this.selectedYear=value;
-      else if(key == 'launch_success')
-      this.selectedLaunch=value;
-      else if(key == 'land_success')
-      this.selectedLand= value;
+    });
+  }
 
-      this.paramSelected.emit({[key]: value});
-    }
+  onfilterClick(key: string, value: string) {
+
+    if (key == 'launch_year')
+    this.selectedYear = value;
+    else if (key == 'launch_success')
+    this.selectedLaunch = value;
+    else if (key == 'land_success')
+    this.selectedLand = value;
+
+    this.paramSelected.emit({ [key]: value });
+  }
 
   ngOnInit(): void {
     this.currentYear = new Date().getFullYear();
-    this.NoOfYears=this.currentYear-this.startYear+1;
+    this.NoOfYears = this.currentYear - this.startYear + 1;
   }
 
 
