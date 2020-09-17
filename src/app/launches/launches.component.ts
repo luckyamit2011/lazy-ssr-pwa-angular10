44b6d1678a0179b23b099de46397b53bd3d2c198
luckyamit2011 <71408232+luckyamit2011@users.noreply.github.com>
2020-09-17 22:22:14 +0530

    Add files via upload

--- src/app/launches/launches.component.ts
+++ src/app/launches/launches.component.ts
@@ -9,42 +9,38 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./launches.component.scss']
 })
 export class LaunchesComponent implements OnInit {
-  public paramSelected = {};
-  NoOfYears:number;
-  currentYear:number;
-  startYear:number = 2006;
-  itemsLimit:number = 100;
+
+  defaultProgramLimit:number = 100;
   programLimit:number;
   selectedYear:string=''; selectedLaunch:string=''; selectedLand:string='';
   public loading: boolean = true;
-  nodata:boolean=false;
-  programs=['start'];
-  filterobj=['limit','launch_year','launch_success','land_success'];
- 
-  params:HttpParams;
+  programs=['start']; //set for first time load
+  
+
   constructor(
     private programService: ProgramService,
     private route: ActivatedRoute,
     private router: Router) { 
        route.queryParams.subscribe( (params) => {
         if(params['limit'])
-        this.programLimit=params['limit']; //set from browser if page load occurs
+        this.programLimit=params['limit']; 
         else
-        this.programLimit=this.itemsLimit;
+        this.programLimit=this.defaultProgramLimit; // default limit if not geting from browser url
         if(params['launch_year'])
         this.selectedYear =  params['launch_year'].toString();
         if(params['launch_success'])
         this.selectedLaunch =  params['launch_success'];
         if(params['land_success'])
         this.selectedLand =  params['land_success'];
-        //console.warn(params);
+
       });
     }
 
   ngOnInit(): void {
-    this.getProgramdata();
+    this.getProgramdata(); /*** load data for first time page load***/
   }
 
+  
   onfilterClick(filter:any){
   
     if(Object.keys(filter)[0] == 'launch_year' )
@@ -54,15 +50,15 @@ export class LaunchesComponent implements OnInit {
     if(Object.keys(filter)[0] == 'land_success' )
     this.selectedLand = Object.values(filter)[0].toString();
 
-    this.getProgramdata();
+    this.getProgramdata(); /*** load data on filter click***/
 
   }
 
   
   getProgramdata(){
-   // this.spinner.show();
+  
     this.loading=true;
-    this.nodata=false;
+   
     let params = new HttpParams()
     .set('limit', this.programLimit.toString());
     
@@ -76,22 +72,18 @@ export class LaunchesComponent implements OnInit {
     
     this.programService.getPrograms(params).subscribe(data => {
       this.programs = data;
-      if(this.programs.length==0)
-      this.nodata=true;
       this.loading=false;
     
-
     //////// for reset router with updated params
-     const paramsArray = params.keys().map(x => ({ [x]: params.get(x) }));
-     let newParam={};
-     paramsArray.forEach(key => {Object.assign(newParam, key)});
+      const paramsArray = params.keys().map(x => ({ [x]: params.get(x) }));
+      let newParam={};
+      paramsArray.forEach(key => {Object.assign(newParam, key)});
 
-     this.router.navigate([], {
+      this.router.navigate([], {
        queryParams: newParam,
        relativeTo: this.route
-   });
-    
-       
+      });
+   
    })
 
   }
